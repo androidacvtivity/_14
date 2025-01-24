2c6e3c768d263317f45c8a7248408a81f4a47b71
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-01-24 16:33:40 +0200

    last modification

--- 340.sql
+++ 340.sql
@@ -22,7 +22,7 @@ FROM CIS2.MD_RIND D
 WHERE
 D.STATUT = '1'  AND  
 ---- 
-D.capitol= 365 AND D.capitol_vers=2008
+D.capitol= 340 AND D.capitol_vers=2004
 and d.rind not in ('--')
   GROUP BY
   D.ID_MD,
@@ -39,7 +39,7 @@ and d.rind not in ('--')
 WHERE
 D.STATUT = '1'  AND  
 ---- 
-D.capitol= 365 AND D.capitol_vers = 2008
+D.capitol= 340 AND D.capitol_vers = 2004
 
 AND D.RIND NOT IN ('-')
 
